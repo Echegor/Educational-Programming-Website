19d37b1146254dee7293d18f209c4dd5db0d95a9
clindgren111 <secretagentchris@gmail.com>
2017-02-03 19:05:43 -0500

    Initial Front End

---
+++ FRONT/login.php
@@ -0,0 +1,32 @@
+<?php
+
+$username= $_REQUEST["ucid"];
+$password = $_REQUEST["password"];
+$postfieldsRaw = array("username" => $username,
+                    "password" => $password);
+                    
+$postfields = json_encode($postfieldsRaw);
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://web.njit.edu/~lme4/index.php',
+    CURLOPT_USERAGENT => 'Test',
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => $postfields,
+));
+
+$result = curl_exec($curl);
+
+curl_close($curl);
+
+$decoded = json_decode($result, true);
+
+echo "<br>";
+echo 'NJIT:' . $decoded['NJIT'] . "<br>"; 
+echo 'BACKEND:' . $decoded['BACKEND'] . "<br><br>"; 
+echo "Information Passed In: <br>";
+echo 'username:' . $postfieldsRaw['username'] . "<br>";
+echo 'password:' . $postfieldsRaw['password'] . "<br><br>";
+echo "Raw Response: <br>" . $result . "<br>";
+?>
