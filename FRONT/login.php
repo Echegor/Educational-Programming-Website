9fd2f0bb64b0779d2e47039a9110848c69b47ea3
clindgren111 <secretagentchris@gmail.com>
2017-02-07 09:19:45 -0500

    Gives clear indication of pass/fail state

--- FRONT/login.php
+++ FRONT/login.php
@@ -22,11 +22,12 @@ curl_close($curl);
 
 $decoded = json_decode($result, true);
 
-echo "<br>";
+/*echo "<br>";
 echo 'NJIT:' . $decoded['NJIT'] . "<br>"; 
 echo 'BACKEND:' . $decoded['BACKEND'] . "<br><br>"; 
 echo "Information Passed In: <br>";
 echo 'username:' . $postfieldsRaw['username'] . "<br>";
 echo 'password:' . $postfieldsRaw['password'] . "<br><br>";
-echo "Raw Response: <br>" . $result . "<br>";
+echo "Raw Response: <br>" . $result . "<br>";*/
+echo $result;
 ?>
