0d63cbbd8a657990a24c5aa9138fcbbf3ab56b87
jamesrpo <jamesrr17@gmail.com>
2017-02-27 16:50:58 -0500

    Update addUser.php

--- BACKEND/addUser.php
+++ BACKEND/addUser.php
@@ -1,35 +1,25 @@
 <?php
-  //Include Configuration File
-  //include('config.php');
+  require_once "config.php";
   
-  //Database connection.
-  $host = "sql1.njit.edu";
-  $user = "jjr27";
-  $pass = "JS6RYpHk";
-  $db = "jjr27";
-  
-  mysql_connect($host, $user, $pass);
-  mysql_select_db($db);
-  
-  
-  if(isset($_POST['username']))
-  {    
-    //JSON response
-    $response = json_decode(file_get_contents('php://input'), true);
+  //JSON response
+  $response = json_decode(file_get_contents('php://input'), true);
     
-    //Values passed.
-    $firstName = $_POST['firstName'];
-    $lastName = $_POSt['lastName'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $roleId = $_POSt['AccountType'];
+  //Values passed.
+  $firstName = $response['firstName'];
+  $lastName = $response['lastName'];
+  $username = $response['username'];
+  $password = $response['password'];
+  $roleId = $response['accountType'];
+    
+  if(isset($roleId))
+  {    
     
     //SQL query tu run against the DB for INSERT.
     $sql = "INSERT INTO TblUser (FirstName, LastName, UserName, Password, RoleID) VALUES('".$firstName."','".$lastName."','".$username."','".$password."', '".$roleId."'");
     
     //Get Result
     $res = mysql_query($sql) or die("Failed to register user in database " .mysql_error());
-    echo $roleId
+
     //Check for returned results
     if($res){      
       //Return json with successful transaction
@@ -45,6 +35,7 @@
       echo json_encode($response);
     }
     
+  }else{
+    echo "No role ID";
   }
-
 ?>
