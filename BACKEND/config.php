8ffd0ea38f3e60d6877a6d20b27a758a0bf51029
jamesrpo <jamesrr17@gmail.com>
2017-02-27 10:08:00 -0500

    Create config.php

---
+++ BACKEND/config.php
@@ -0,0 +1,15 @@
+//James Restrepo
+//CS-490
+//Back End - ConfigFile
+//Spring 2017
+
+<?php
+  //Database connection.
+  $host = "sql1.njit.edu";
+  $user = "jjr27";
+  $pass = "JS6RYpHk";
+  $db = "jjr27";
+
+  mysql_connect($host, $user, $pass);
+  mysql_select_db($db);
+?>
