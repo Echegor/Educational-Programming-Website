f013fd2a0e45eeb62e3a2fdf2f86a7a139e13ac3
jamesrpo <jamesrr17@gmail.com>
2017-03-03 16:43:44 -0500

    Update config.php

--- BACKEND/config.php
+++ BACKEND/config.php
@@ -1,15 +1,16 @@
-//James Restrepo
-//CS-490
-//Back End - ConfigFile
-//Spring 2017
-
 <?php
+  //James Restrepo
+  //CS-490
+  //Back End - ConfigFile
+  //Spring 2017
+
   //Database connection.
   $host = "sql1.njit.edu";
   $user = "jjr27";
   $pass = "JS6RYpHk";
   $db = "jjr27";
 
-  mysql_connect($host, $user, $pass);
-  mysql_select_db($db);
+  //MySQL Connection
+  $connection = mysqli_connect($host,$user,$pass,$db) or die("Error " . mysqli_error($connection));
+
 ?>
